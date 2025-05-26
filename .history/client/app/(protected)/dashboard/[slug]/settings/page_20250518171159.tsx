c7b8de7e41b6519f0478284d418ba3dc906746e7
abdulrg01 <105718377+abdulrg01@users.noreply.github.com>
2025-05-26 14:52:18 +0100

    Delete .history directory

--- .history/client/app/(protected)/dashboard/[slug]/settings/page_20250518171159.tsx
+++
@@ -1,8 +0,0 @@
-import Billing from "@/components/global/billing";
-import React from "react";
-
-const page = () => {
-  return <Billing />;
-};
-
-export default page;
--- .history/client/app/(protected)/dashboard/page_20250518170111.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-async function page() {
-  return (
-    <div>page</div>
-  )
-}
-
-export default page
